1eef3c846abde0b63cd1b99bac7f639ed839ed43
Simranjeet <simran7009191049@gmail.com>
2024-06-28 00:06:17 +0530

    bug fix

--- src/components/URLs.jsx
+++ src/components/URLs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const URLs = ({name, address}) => {
   return (
-    <div className="bg-gray-800 w-20 h-20 rounded-lg mr-4 flex justify-center p-2 items-center ">
+    <div className="bg-gray-800 w-20 h-20 rounded-lg mr-4 my-4 flex justify-center p-2 items-center ">
       <div className=""></div>
       <a href={address} target='_blank' className="text-blue-400 hover:underline max-w-20 text-wrap ">{name}</a>
     </div>
